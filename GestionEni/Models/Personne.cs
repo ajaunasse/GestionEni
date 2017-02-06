7c85e0fd68bf93f8d5b9b63b41cd315971ad7800
Alexandre Jaunasse <alexandre.jaunasse@gmail.com>
2017-02-06 16:02:46 +0100

    First commit

---
+++ GestionEni/Models/Personne.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GestionEni.Models
+{
+    public class Personne
+    {
+        private string firstname;
+        public string Firstname
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+
+        private string lastname;
+        public string Lastname
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+
+        [Required]
+        private string username;
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        [Required]
+        [DataType(DataType.Password)]
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+    }
+}
